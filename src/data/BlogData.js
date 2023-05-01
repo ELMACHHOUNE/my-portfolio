346ed59a048edab82a70dff7425936f176795fd2
MOHAMED EL MACHHOUNE <mohamed20rida@gmail.com>
2023-05-01 01:32:09 +0100

    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,30 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"I have successfully completed The Full-Stack Javascript Bootcamp course at GOMYCODE and received my certificate",
+        tags:["Full-stack web developer"],
+        date:"9 Jan to 24 May, 2023",
+        imgSrc:"https://media.licdn.com/dms/image/D4E22AQGQK7M2j-2drg/feedshare-shrink_1280/0/1682439780455?e=1685577600&v=beta&t=3-xUj9hxwFkEFls9XY4CrM54HsYRPvitm4auNvov-Yw",
+        link:"https://www.linkedin.com/posts/mohamed-el-machhoune_gomycode-certificate-achievement-activity-7056663908161314816-SVeF?utm_source=share&utm_medium=member_desktop"
+    },
+    {
+        id:2,
+        name:"I have successfully completed The Job Hunters Guide course at GOMYCODE and received my certificate",
+        tags:["Linkedin", "Interview", "C.V"],
+        date:"9 Feb, 2023 to 23 Mar, 2024",
+        imgSrc:"https://media.licdn.com/dms/image/D4E22AQEQZiHI267h5A/feedshare-shrink_1280/0/1681496016469?e=1685577600&v=beta&t=dAJko53QzndbcV5fH5fyZ6SiDAw0LrztOtrO_H_pJ_k",
+        link:"https://www.linkedin.com/posts/mohamed-el-machhoune_gomycode-odc-jobsearch-activity-7052705472826478592-D76G?utm_source=share&utm_medium=member_desktop"
+    },
+    {
+        id:3,
+        name:"I have successfully completed The UX/UI Design course at GOMYCODE and received my certificate",
+        tags:["UX", "UI", "Figma"],
+        date:"2 Feb, 2023",
+        imgSrc:"https://media.licdn.com/dms/image/D4E22AQFDDJY_58lIqQ/feedshare-shrink_1280/0/1680778894055?e=1685577600&v=beta&t=zK8BQPiWzRFw45ip_xEUKFPexe117EN5L1J4vLsOS_o",
+        link:"https://www.linkedin.com/posts/mohamed-el-machhoune_jai-le-plaisir-de-vous-annoncer-que-jai-activity-7049697645132410880-MWUO?utm_source=share&utm_medium=member_desktop"
+    }
+
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,82 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Agency Landing Page",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","styledComponents"],
+
+        demo:"https://agency-mrtechnologies.netlify.app/",
+        github:"https://github.com/ELMACHHOUNE/agency-mrtechnologies"
+    },
+    {
+        id:2,
+        name:"Job listing website",
+        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
+        tags:["react","sass"],
+        
+        demo:"https://job-listing-website.netlify.app/",
+        github:"https://github.com/ELMACHHOUNE/job-listing-website-using-react"
+        
+    },
+    {
+        id:3,
+        name:"Movie app website",
+        description:"A movie app using react router and hooks with localstorage.",
+        tags:["React.js","React router","Localstorage"],
+        
+        demo:"https://movie-app-elmachhoune.netlify.app/",
+        github:"https://github.com/ELMACHHOUNE/React-Router-Checkpoint"
+    },
+    {
+        id:4,
+        name:"Sidebar Menu",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://sidebar-using-react.netlify.app/",
+        github:"https://github.com/ELMACHHOUNE/sidebar-using-react"
+       
+    },{
+        id:5,
+        name:"Todo App(React + Redux)",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["react","redux","framer-motion"],
+        
+        demo:"https://elmachhoune-todo-app-using-redux.netlify.app/",
+        github:"https://github.com/ELMACHHOUNE/todo-app-using-redux"
+        
+    },
+    {
+        id:6,
+        name:"Responsive Navigation Menu",
+        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+        tags:["react","react-router","css"],
+        
+        demo:"https://responsive-navbar-using-react.netlify.app/",
+        github:"https://github.com/ELMACHHOUNE/responsive-navbar-using-react"
+        
+    },
+    {
+        id:7,
+        name:"Inventory management (desktop app)",
+        description:"A desktop app using C# and SQL server (exactly ADO.net framework).",
+        tags:["ADO.net","SQL server","C#"],
+        
+        demo:"https://youtu.be/3e4UknKLN-E",
+        github:"https://github.com/ELMACHHOUNE/responsive-navbar-using-react"
+        
+    },
+    {
+        id:8,
+        name:"Hotel management system (desktop app)",
+        description:"A desktop app using C# and MySQL (exactly ADO.net framework and phpMyAdmin).",
+        tags:["ADO.net","MySQL","C#","Entity framework"],
+        
+        demo:"https://youtu.be/X2iI9gKzrkQ",
+        github:"https://github.com/ELMACHHOUNE/hotel-management-project"
+        
+    },
+  
+] 
