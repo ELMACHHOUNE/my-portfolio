ea6e5873bc541626e114fb5ff10c7e353f16d08e
MOHAMED EL MACHHOUNE <91902222+ELMACHHOUNE@users.noreply.github.com>
2023-05-19 11:12:27 +0100

    Update BlogData.js

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -3,7 +3,7 @@
 export const Blogs = [
     {
         id:1,
-        name:"I have successfully completed The Full-Stack Javascript Bootcamp course at GOMYCODE and received my certificate",
+        name:"I have successfully completed The Full-Stack Javascript Bootcamp course at Orange digital center Rabat collaborate with GOMYCODE and received my certificate",
         tags:["Full-stack web developer"],
         date:"9 Jan to 24 May, 2023",
         imgSrc:"https://media.licdn.com/dms/image/D4E22AQGQK7M2j-2drg/feedshare-shrink_1280/0/1682439780455?e=1685577600&v=beta&t=3-xUj9hxwFkEFls9XY4CrM54HsYRPvitm4auNvov-Yw",
@@ -11,7 +11,7 @@ export const Blogs = [
     },
     {
         id:2,
-        name:"I have successfully completed The Job Hunters Guide course at GOMYCODE and received my certificate",
+        name:"I have successfully completed The Job Hunters Guide course at Orange digital center Rabat collaborate with GOMYCODE and received my certificate",
         tags:["Linkedin", "Interview", "C.V"],
         date:"9 Feb, 2023 to 23 Mar, 2024",
         imgSrc:"https://media.licdn.com/dms/image/D4E22AQEQZiHI267h5A/feedshare-shrink_1280/0/1681496016469?e=1685577600&v=beta&t=dAJko53QzndbcV5fH5fyZ6SiDAw0LrztOtrO_H_pJ_k",
@@ -19,11 +19,11 @@ export const Blogs = [
     },
     {
         id:3,
-        name:"I have successfully completed The UX/UI Design course at GOMYCODE and received my certificate",
+        name:"I have successfully completed The UX/UI Design course at Orange digital center Rabat collaborate with GOMYCODE and received my certificate",
         tags:["UX", "UI", "Figma"],
-        date:"2 Feb, 2023",
+        date:"2 Feb to 24 May, 2023",
         imgSrc:"https://media.licdn.com/dms/image/D4E22AQFDDJY_58lIqQ/feedshare-shrink_1280/0/1680778894055?e=1685577600&v=beta&t=zK8BQPiWzRFw45ip_xEUKFPexe117EN5L1J4vLsOS_o",
-        link:"https://www.linkedin.com/posts/mohamed-el-machhoune_jai-le-plaisir-de-vous-annoncer-que-jai-activity-7049697645132410880-MWUO?utm_source=share&utm_medium=member_desktop"
+        link:"https://www.linkedin.com/posts/mohamed-el-machhoune_ux-uidesign-certificateachievement-activity-7065248571792453634-oqA5?utm_source=share&utm_medium=member_desktop"
     }
 
   
